61a9e2a3cae165d04fb160b1351740a5c71e898d
Shashank Date <stdate@multiservice.com>
2017-02-06 23:04:24 -0600

    Modified mruby code

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,5 @@
 def fibo(n)
-  return 1 if n == 0
+  return 0 if n == 0
   return 1 if n == 1
   fibo(n-1) + fibo(n-2)
 end
--- mrb_fibo.c
+++ mrb_fibo.c
@@ -1,10 +1,14 @@
 #include "mruby.h"
+#include <mruby/compile.h>
+/*
 #include "mruby/irep.h"
 #include "fibonacci.c"
+*/
  int main(void)
  {
    mrb_state *mrb = mrb_open();
-   if (!mrb) {/* handle error */}
-   mrb_load_irep(mrb, fibo_sym);
+   if (!mrb) {printf("Error opening mrb\n");}
+   /* mrb_load_irep(mrb, fibo_sym); */
+   mrb_load_string(mrb, "def f(n); n<2 ? n : f(n-1)+f(n-2); end; p(f(32));");
    mrb_close(mrb);
  }

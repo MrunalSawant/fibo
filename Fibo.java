bac5fc1fce0982726a7bf24c734dee2bdf9eb5fd
MrunalSawant <mrunalsawant7@gmail.com>
2017-02-09 10:24:14 +0530

    Fibo.java
    javac Fibo.java //creates class file 
    java Fibo

---
+++ Fibo.java
@@ -0,0 +1,13 @@
+public class Fibo {
+ 	public static void main(String[] args) {
+	    System.out.println(fibonacci(32));
+  	} 	
+
+  	public static int fibonacci(int n){
+ 			    if (n == 0) { return 0; }
+  			  if (n == 1) { return 1; }
+  		return fibonacci(n - 2) + fibonacci(n - 1);
+	}
+
+}
+
